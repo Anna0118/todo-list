267d1d1764547b5f172c0b0593a241d013713414
Anna0118 <annachu0988@gmail.com>
2022-12-18 11:17:17 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
+const mongoose = require("mongoose"); // 載入mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const app = express();
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); // 設定連線到 mongoDB
+
+const db = mongoose.connection;
+// 用on註冊監聽器，監聽error事件有沒有發生
+// 只要觸error就印出error訊息
+db.on("error", () => {
+  console.log("mongodb error");
+});
+// 針對連線成功的open其情況
+// 連線成功只會發生一次，所以特地用once，一但成功後，在執行callback以後就會解除監聽器
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 // 設定首頁路由
 app.get("/", (req, res) => {
---
+++ models/todo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema; // 使用mongoose的Schema模組
+
+const todoSchema = new Schema({
+  // 建構一個新的Schema
+  name: {
+    type: String, // 資料型別是字串
+    required: true, // 這是個必填欄位
+  },
+  done: {
+    type: Boolean,
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
