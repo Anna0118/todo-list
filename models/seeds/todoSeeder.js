ccf9094a1c0af3e76ee277b6bdce98838b749237
Anna0118 <annachu0988@gmail.com>
2022-12-18 16:11:10 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Todo = require("../todo"); // 載入 todo model
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+// 使空callback概念
+db.once("open", () => {
+  console.log("mongodb connected!");
+  for (let i=0; i<10;i++){
+    Todo.create({
+        name:`name-${i}`
+    })
+    console.log('done')
+  }
+});
+
+
