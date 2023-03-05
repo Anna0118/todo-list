965014db62e22f561e7086ee656e068ed09d4e35
Anna0118 <annachu0988@gmail.com>
2023-03-05 17:23:27 +0800

    fix markdown

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
-// 載入 express 並建構應用程式伺服器
+// 載入相關模組
 const express = require("express");
 const mongoose = require("mongoose"); // 載入mongoose
 const exphbs = require("express-handlebars"); // 載入樣版引擎Handlebars
-// 引用 body-parser
-const bodyParser = require("body-parser");
-
+const bodyParser = require("body-parser"); // 引用 body-parser
 const Todo = require("./models/todo"); // 載入Todo model
-const app = express();
-// 建立一個叫hbs的樣版引擎, 傳入相關參數
-// 指定副檔名為 .hbs，才能把預設的長檔名改寫成短檔名
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-// 啟用樣版引擎
-app.set("view engine", "hbs");
-
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+// connest to mongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); // 設定連線到 mongoDB
-
+});
+// set db
 const db = mongoose.connection;
-// 用on註冊監聽器，監聽error事件有沒有發生
 // 只要觸error就印出error訊息
 db.on("error", () => {
   console.log("mongodb error");
 });
-// 針對連線成功的open其情況
-// 連線成功只會發生一次，所以特地用once，一但成功後，在執行callback以後就會解除監聽器
 db.once("open", () => {
   console.log("mongodb connected!");
 });
 
-// setting route
+const app = express();
+const port = 3000;
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" })); // set template engine
+app.set("view engine", "hbs"); // 啟用樣版引擎
+app.use(bodyParser.urlencoded({ extended: true })); // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+
+// route setting
 app.get("/", (req, res) => {
   // 拿到全部的todo資料
   Todo.find() // 從資料庫找出資料 (目前沒有傳入參數，所以會撈出整份資料)
@@ -114,6 +107,6 @@ app.post("/todos/:id/delete", (req, res) => {
 });
 
 // 設定 port 3000
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("App is running on http://localhost:3001");
 });
